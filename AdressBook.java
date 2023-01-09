5a5084bf289e0ef8a90a8f23bac57a03bee4f716
amod1811 <ammch101@gmail.com>
2023-01-09 12:08:02 +0530

    Added : program to AddressBOOk create the contact

---
+++ AdressBook.java
@@ -0,0 +1,10 @@
+package bl.com.addressbooksystem;
+
+public class AdressBook {
+	private String firstname;
+	private String lastname;
+	private int mobilenumber;
+	private String email;
+	private String city;
+	private String state;
+}
