57f6fd90ffac8b9502a77a1adb4c92abb3cf401e
amod1811 <ammch101@gmail.com>
2023-01-09 13:08:44 +0530

    Added : Program to pojo class

--- ContactPerson.java
+++ ContactPerson.java
@@ -7,6 +7,7 @@ public class ContactPerson {
 	private String email;
 	private String city;
 	private String state;
+
 	public ContactPerson() {
 		this.firstname = firstname;
 		this.lastname = lastname;
@@ -14,45 +15,57 @@ public class ContactPerson {
 		this.email = email;
 		this.city = city;
 		this.state = state;
-		
-		
+
 	}
+
 	public String getFirstname() {
 		return firstname;
 	}
+
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
+
 	public String getLastname() {
 		return lastname;
 	}
+
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
+
 	public int getMobilenumber() {
 		return mobilenumber;
 	}
+
 	public void setMobilenumber(int mobilenumber) {
 		this.mobilenumber = mobilenumber;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public String getState() {
 		return state;
 	}
+
 	public void setState(String state) {
 		this.state = state;
 	}
+
 	@Override
 	public String toString() {
 		return "Contact_Person [firstname=" + firstname + ", lastname=" + lastname + ", mobilenumber=" + mobilenumber
